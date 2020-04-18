f78cb7da3cbc63f4e5f950e1c249639290c97306
Kronos2308 <36446521+Kronos2308@users.noreply.github.com>
2020-04-18 01:04:22 +0100

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -2,6 +2,6 @@
 make
 echo completado
 echo %cd%
-move /y output\boot-menu.bin "C:\Users\Administrador\Documents\GitHub\Kaiser\Kaiser\payload.bin"
+move /y output\boot-menu.bin "..\Kaiser\payload.bin"
 %systemroot%\system32\timeout.exe 10
 
